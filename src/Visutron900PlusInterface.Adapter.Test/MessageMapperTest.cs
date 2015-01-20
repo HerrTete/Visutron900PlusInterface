d8b38147514c63a02b5913937ce7c08ee5455ae7
Rico <rico.tetmeyer@gmail.com>
2015-01-20 07:47:54 +0100

    TestProjectAdded

---
+++ src/Visutron900PlusInterface.Adapter.Test/MessageMapperTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Visutron900PlusInterface.Adapter.Test
+{
+    [TestClass]
+    public class MessageMapperTest
+    {
+        [TestMethod]
+        public void MappToInMessageTest()
+        {
+        }
+
+        [TestMethod]
+        public void MapToOutMessageTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapFromOutMessageTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapFromInMessageTest()
+        {
+
+        }
+    }
+}
